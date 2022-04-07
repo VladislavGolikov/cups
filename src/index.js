f4a9f9a13887ef7ee20986b7fc393495387864a6
Vladislav Golikov <vladislav.v.golikov@gmail.com>
2022-04-07 14:04:38 +0300

    в проект добавлена точка входа - простейший индекс.жс, при запуске в пакаджейсон автоматом добавлены целевые браузеры

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+/*import './index.scss';
+
+
+import {Cup} from './index2.jsx';
+
+import cups from './config.json';
+
+
+
+
+
+let vava=new Cup(cups.cups[0]);
+
+alert(Object.entries(vava.render().props))*/
+const vava=`первая проба...`
+
+
+ReactDOM.render(
+      vava,
+      document.getElementById('root')
+    );
+
+
+
+
+
+
+
+
