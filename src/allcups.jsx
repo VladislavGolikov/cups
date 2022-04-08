deb850e313287aec05cd8d93fbe579811aa596d8
Vladislav Golikov <vladislav.v.golikov@gmail.com>
2022-04-08 18:09:55 +0300

    добавлены стили в scss и автоматический путь к картинке в компоненте

--- src/allcups.jsx
+++ src/allcups.jsx
@@ -2,20 +2,21 @@ import React from 'react';
 
 import cups from './allcups.json';
 
-
+/*cltkfnm расшифровку массива описания чтобы обращаться по имени сущности!*/
 let bebe=1;
 
 
 export class Cup extends React.Component {
     constructor(props) {
         super(props);
-        this.filePath=`cups.cups[0][0]`;
+        this.filePath=cups.cups[0][8];
+
         this.state= {};
         this.cupName=cups.cups[0][1];
     }
 
 render() {
-    return <div className="cup" style={{backgroundImage: `url(${process.env.PUBLIC_URL}/${bebe}.png)`}}>
+    return <div className="cup" style={{backgroundImage: `url(${process.env.PUBLIC_URL}/${this.filePath}2.png)`}}>
 
     {this.cupName}</div>;
   }
