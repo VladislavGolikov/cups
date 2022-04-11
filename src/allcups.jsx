685ce36615e33764f8263d5979cde559e4b53933
Vladislav Golikov <vladislav.v.golikov@gmail.com>
2022-04-11 17:32:25 +0300

    проведена частичная предварительная стилизация компонента на гриде

--- src/allcups.jsx
+++ src/allcups.jsx
@@ -1,25 +1,42 @@
 import React from 'react';
 
-import cups from './allcups.json';
+import allcups from './allcups.json';
 
-/*cltkfnm расшифровку массива описания чтобы обращаться по имени сущности!*/
-let bebe=1;
 
 
 export class Cup extends React.Component {
     constructor(props) {
         super(props);
-        this.filePath=cups.cups[0][8];
+        /* в соответствии с полями как в allcups.fields: */
+        this.id=allcups.cups[props][0];
+        this.title=allcups.cups[props][1];
+        this.color=allcups.cups[props][2];
+        this.material=allcups.cups[props][3];
+        this.date=allcups.cups[props][4];
+        this.source=allcups.cups[props][5];
+        this.land=allcups.cups[props][6];
+        this.place=allcups.cups[props][7];
+        this.fileName=allcups.cups[props][8];
+
+        this.fileFrames=['1.png','2.png','3.png','4.png'];
+
 
         this.state= {};
-        this.cupName=cups.cups[0][1];
+
     }
 
-render() {
-    return <div className="cup" style={{backgroundImage: `url(${process.env.PUBLIC_URL}/${this.filePath}2.png)`}}>
-
-    {this.cupName}</div>;
-  }
+    render() {
+        let filePath=`${process.env.PUBLIC_URL}/${this.fileName}${this.fileFrames[0]}`;
+        return <div className="cup">
+                <div className="id">{'№ '+this.id}</div>
+                <div className="title">{this.title}</div>
+                <div className="material">{this.material}</div>
+                <div className="date">{this.date}</div>
+                <div className="land">{this.land}</div>
+                <div className="place">{this.place}</div>
+                <div className="picture" style={{backgroundImage: `url(${filePath})`}}></div>
+               </div>;
+    }
 
 
 
--- src/index.js
+++ src/index.js
@@ -4,20 +4,17 @@ import './index.scss';
 
 import {Cup} from './allcups.jsx';
 
-import cups from './allcups.json';
 
 
-alert(new Cup(cups.cups[0]))
 
 
-let vava=new Cup(cups.cups[0]);
+const firstComponent=new Cup(6);
 
-/*alert(Object.entries(vava.render().props))*/
-//const vava=`первая проба...`
+
 
 
 ReactDOM.render(
-      vava.render(),
+      firstComponent.render(),
       document.getElementById('root')
     );
 
