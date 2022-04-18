a1b3b3d45e6f89db242aee23dc5e0b164843619f
Vladislav Golikov <vladislav.v.golikov@gmail.com>
2022-04-18 17:19:19 +0300

    файлы разложены по папкам - стили, компоненты и дата. цепочка компонент теперь все кружки - кружка - данные. отрисовываются сразу все кружки

---
+++ src/components/cup.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+
+import allcups from '../data_source/allcups.json';
+
+
+
+export class Cup extends React.Component {
+    constructor(props) {
+        super(props);
+        /* в соответствии с полями как в allcups.fields: */
+        this.id=allcups.cups[props.serial][0];
+        this.title=allcups.cups[props.serial][1];
+        this.color=allcups.cups[props.serial][2];
+        this.material=allcups.cups[props.serial][3];
+        this.date=allcups.cups[props.serial][4];
+        this.source=allcups.cups[props.serial][5];
+        this.land=allcups.cups[props.serial][6];
+        this.place=allcups.cups[props.serial][7];
+        this.fileName=allcups.cups[props.serial][8];
+
+        this.fileFrames=['1.png','2.png','3.png','4.png'];
+
+        this.vava=0
+
+
+
+        this.state= {counter:0};
+
+    }
+
+    render() {
+        let filePath=`${process.env.PUBLIC_URL}/${this.fileName}${this.fileFrames[this.vava]}`;
+        return <div className="cup" onClick={this.counterPlus.bind(this)}>
+                <div className="id">{'№ '+this.id}</div>
+                <div className="title">{this.title}</div>
+                <div className="material">{this.material}</div>
+                <div className="date">{this.date}</div>
+                <div className="land">{this.land}</div>
+                <div className="place">{this.place}</div>
+                <div className="picture" style={{backgroundImage: `url(${filePath})`}}></div>
+               </div>;
+    }
+
+counterPlus() {
+
+
+  this.vava+=1;
+
+
+
+
+
+    ;
+    if (this.vava>=this.fileFrames.length) {this.vava=0}
+        console.log(this.vava);
+
+
+
+}
+}
+
+
--- src/index.js
+++ src/index.js
@@ -1,22 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.scss';
 
-import {Cup} from './allcups.jsx';
+import {AllCups} from './components/allcups.jsx';
 
 
+const root=ReactDOM.createRoot(document.getElementById("root"));
 
 
+const firstComponent=new AllCups(0);
 
-const firstComponent=new Cup(6);
-
-
-
-
-ReactDOM.render(
-      firstComponent.render(),
-      document.getElementById('root')
-    );
+root.render(firstComponent.render());
 
 
 
