551b375d005d38cce91e698908c3e137fe3281ee
Vladislav Golikov <vladislav.v.golikov@gmail.com>
2022-05-03 19:35:45 +0300

    тернарный оператор передан прямо аргументом в setstate

--- src/components/cup.jsx
+++ src/components/cup.jsx
@@ -5,18 +5,13 @@ import allcups from '../data_source/allcups.json';
 
 const fileFrames=['1.png','2.png','3.png','4.png'];
 
-function cycle(value) {
-    return ++value>=fileFrames.length ? 0 : value
-}
-
-
 export const Cup=(props) => {
     let [value, setState]=useState(0);
     const [id,title,color,material,date,source,land,place,fileName]=allcups.cups[props.serial];
     let filePath=`${process.env.PUBLIC_URL}/${fileName}${fileFrames[value]}`;
 
     return (
-        <div className="cup" onClick={()=>{setState(cycle(value))}}>
+        <div className="cup" onClick={()=>{setState(++value>=fileFrames.length ? 0 : value)}}>
             <div className="id">{'№ '+id}</div>
             <div className="title">{title}</div>
             <div className="material">{material}</div>
