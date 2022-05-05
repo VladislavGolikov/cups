f66f7c9a5bba0edf2650535104fca0a150a6e12f
Vladislav Golikov <vladislav.v.golikov@gmail.com>
2022-05-05 05:02:55 +0300

    стили расчитываются динамически исходя из количества кружек, высоты и ширины экрана; компонета allcups сама формирует свой грид-шаблон

--- src/components/allcups.jsx
+++ src/components/allcups.jsx
@@ -4,12 +4,25 @@ import {Cup} from './cup.jsx';
 
 import allcups from '../data_source/allcups.json';
 
-// alert(window.innerHeight/window.innerWidth)
+
 
 export const AllCups=() => {
+    const padding=1;
+    const columns=allcups.cups.reduce((prev,el,ind,arr)=>{
+        const value=Math.abs((ind+1)/Math.ceil(arr.length/(1+ind))-window.innerWidth/window.innerHeight);
+        if (prev[0]>value) {return [value,ind]}else{return prev};
+    },[1000,0])[1];
+    const rows=Math.ceil(allcups.cups.length/columns);
+    const size=Math.min(Math.floor(window.innerHeight/rows)-padding*2,Math.floor(window.innerWidth/columns)-padding*2)
+
     return (
-        <div className='cups'>
-        {allcups.cups.map((el,ind)=>{return <Cup serial={ind} key={allcups.cups[ind][0]}></Cup>})}
+        <div className='cups' style={{
+            gridTemplateRows: `repeat(${rows}, ${size}px)`,
+            gridTemplateColumns: `repeat(${columns}, ${size}px)`,
+            padding: `${padding}px`
+        }}>
+        {allcups.cups.map((el,ind)=>{return <Cup serial={ind} rows={rows} columns={columns} key={allcups.cups[ind][0]}></Cup>})}
         </div>
     )
 }
+
--- src/components/cup.jsx
+++ src/components/cup.jsx
@@ -9,11 +9,16 @@ export const Cup=(props) => {
     let [value, setState]=useState(0);
     const [id,title,color,material,date,source,land,place,fileName]=allcups.cups[props.serial];
     let filePath=`${process.env.PUBLIC_URL}/${fileName}${fileFrames[value]}`;
+    const left=(props.serial)%props.columns+1;
+    const top=Math.ceil((props.serial+1)/props.columns);
 
     return (
-        <div className="cup" onClick={()=>{setState(++value>=fileFrames.length ? 0 : value)}}>
+        <div className="cup"
+             onClick={()=>{setState(++value>=fileFrames.length ? 0 : value)}}
+             style={{gridRow: `${top}`, gridColumn: `${left}`}}>
+
             <div className="id">{'№ '+id}</div>
-            <div className="title">{title}</div>
+            <div className="title">{title.toLowerCase()}</div>
             <div className="material">{material}</div>
             <div className="date">{date}</div>
             <div className="land">{land}</div>
